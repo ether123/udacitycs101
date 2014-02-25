9ad149611f636b95ff960728aafebc4761c3653b
Shradha Gaikwad <g.shradha@gmail.com>
2014-02-25 11:55:46 -0500

    Unit 1 Problem Set

---
+++ Unit 1 Problem Set/1.1_numberofhours.py
@@ -0,0 +1,8 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+hours_in_day = 24
+days_in_seven_weeks = 7 * 7
+
+number_of_hours = days_in_seven_weeks * hours_in_day
+print number_of_hours
+
+#>>> 1176 
