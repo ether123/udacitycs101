bced728a42be3798ed62a1239679323c2d7fa55d
Shradha Gaikwad <g.shradha@gmail.com>
2014-02-25 12:03:52 -0500

    Problem set 1

---
+++ Unit 1 Problem Set/1.3_concatenation.py
@@ -0,0 +1,11 @@
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+print s[0:1]+t[2:] #concatenating substring in variable t to the first two characters of s.
+
+
+>>>udacious
