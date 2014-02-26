4f40180d8236170108693c1b47216ea59958915d
Shradha Gaikwad <g.shradha@gmail.com>
2014-02-26 00:04:26 -0500

    return greatest of inputs

---
+++ Unit 3 Problem Set/3.2_return_max.py
@@ -0,0 +1,22 @@
+# Define a procedure, greatest,
+# that takes as input a list
+# of positive numbers, and
+# returns the greatest number
+# in that list. If the input
+# list is empty, the output
+# should be 0.
+
+def greatest(list):
+    if not list:
+        return 0
+    else:
+        return max(list)
+
+
+
+
+
+print greatest([4,23,1])
+#>>> 23
+print greatest([])
+#>>> 0
